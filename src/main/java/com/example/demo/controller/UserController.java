30eff2ffa3df7934a0a5895ccad85cdeb175e23e
LW <liu.wei@wuxi.stee.stengg.com.cn>
2019-04-30 17:35:21 +0800

    update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,11 +6,8 @@ import com.example.demo.server.ItemServer;
 import com.example.demo.server.UserServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
